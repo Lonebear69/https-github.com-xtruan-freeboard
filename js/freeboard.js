dd6d7b8a421dd3fd84194edd91c7912ed939c2b5
Struan Clark <xtruan@gmail.com>
2022-05-13 13:43:03 -0600

    updated freeboard with new plugins in default config

--- js/freeboard.js
+++ js/freeboard.js
@@ -2273,6 +2273,8 @@ function WidgetModel(theFreeboardModel, widgetPlugins) {
 			if (!_.isUndefined(self.widgetInstance) && _.isFunction(self.widgetInstance.onCalculatedValueChanged) && !_.isUndefined(returnValue)) {
 				try {
 					self.widgetInstance.onCalculatedValueChanged(settingName, returnValue);
+					// Force height refresh
+					self._heightUpdate.valueHasMutated();
 				}
 				catch (e) {
 					console.log(e.toString());
